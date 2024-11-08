cae4e58e2fb95282d7de0ffdde792afc960f1898
John Stoklas <jstoklas@trinity.edu>
2024-11-08 12:22:27 -0600

    work on story 2

--- Connect4.hs
+++ Connect4.hs
@@ -7,3 +7,9 @@ data Winner = Tuple (Player, [Position]) | Game
 --Type declaration
 type Move = Int
 type Game = [[Position]]
+
+wonGame :: Game -> Winner
+wonGame game = [aux columns (head columns) 0 | columns <- game]
+    where aux xs prev 4 = (prev, xs)
+          aux [] _ _ = game
+          aux (x:xs) prev count = if x == prev then aux xs prev (count+1) else aux xs x 0
