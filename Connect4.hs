9045742a2f616e06798b6d3bba8e5ecc028c7611
Christian Walters <christian@Christians-Mac-Daddy.local>
2024-11-08 14:21:45 -0600

    Working checkMove

--- Connect4.hs
+++ Connect4.hs
@@ -7,3 +7,13 @@ data Winner = Tuple (Player, [Position]) | Game deriving (Eq, Show)
 --Type declaration
 type Move = Int
 type Game = [[Position]]
+
+{-Indexing for board positions with this code starts at 1, i.e. 
+  Game is assumed to be the set with indexes 1,2,3,4,5,6,7
+  NOT 0,1,2,3,4,5,6 
+  TODO: figure out how to get this to work with 0 indexing
+        if we decide thats what we want-}
+checkMove :: Game -> Move -> Bool
+checkMove [] _ = error "Column not in board"
+checkMove (x:_) 1 = Empty `elem` x
+checkMove (x:xs) move = checkMove xs (move-1)
