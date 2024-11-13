db54179f7dafcaaff26e0556fc2d2fc2a94267ac
John Stoklas <jstoklas@trinity.edu>
2024-11-13 15:21:35 -0600

    added tie

--- Connect4.hs
+++ Connect4.hs
@@ -12,7 +12,7 @@ data Position = Player Player | Empty deriving (Eq, Show)
 --QUESTION: instead of having a tuple (Player, [Positions]), could we just have the winning player color?
 --          the set of positions could be complicated to grab, and I don't think we will even do anything with it
 --          its not like we have the cords stored to change anything in our output on a win
-data Winner = Winner Player | None deriving (Eq, Show)
+data Winner = Winner Player | Tie | Ongoing deriving (Eq, Show)
 
 --Type declaration
 --Move is the integer value of what column the move is trying to be placed in, should be in range 1-7 always
@@ -103,20 +103,29 @@ spaceInCol (x:_) = x == Empty
 -- runs through all posibilities of vertical horizontal and diagonal win states, if any find a
 -- winner it is returned
 wonGame :: Board -> Winner
-wonGame game =
-    let vertical = [checkFourDown columns | columns <- game]
-        horizontal = helperHorizontal game
-        diagonalDown = helperDiagonalDown game
-        diagonalUp = helperDiagonalUp game
-    in if Winner Yellow `elem` vertical || Winner Yellow == horizontal || Winner Yellow == diagonalDown || Winner Yellow == diagonalUp
+wonGame board =
+    let vertical = [checkFourDown columns | columns <- board]
+        horizontal = helperHorizontal board
+        diagonalDown = helperDiagonalDown board
+        diagonalUp = helperDiagonalUp board
+        potentialWinner = (diagonalUp:diagonalDown:horizontal:vertical)
+    in if Winner Yellow `elem` potentialWinner
        then Winner Yellow
-       else if Winner Red `elem` vertical || Winner Red == horizontal || Winner Red == diagonalDown || Winner Red == diagonalUp
+       else if Winner Red `elem` potentialWinner
        then Winner Red
-       else None
+       else fullBoard board
+
+fullBoard :: Board -> Winner
+fullBoard board = if Ongoing `elem` [fullRow row | row <- board] then Ongoing else Tie
+
+fullRow :: [Position] -> Winner
+fullRow [] = Tie
+fullRow (Empty:xs) = Ongoing
+fullRow (_:xs) = fullRow xs
 
 -- checks if there are four in a row vertically
 checkFourDown :: [Position] -> Winner
-checkFourDown [] = None
+checkFourDown [] = Ongoing
 checkFourDown (Player Red:Player Red:Player Red:Player Red:xs) = Winner Red
 checkFourDown (Player Yellow:Player Yellow:Player Yellow:Player Yellow:xs) = Winner Yellow
 checkFourDown (x:xs) = checkFourDown xs
@@ -126,30 +135,30 @@ checkFourAcross :: [Position] -> [Position] -> [Position] -> [Position] -> Winne
 checkFourAcross (Player Red:_) (Player Red:_) (Player Red:_) (Player Red:_) = Winner Red
 checkFourAcross (Player Yellow:_) (Player Yellow:_) (Player Yellow:_) (Player Yellow:_) = Winner Yellow
 checkFourAcross (_:lst1) (_:lst2) (_:lst3) (_:lst4) = checkFourAcross lst1 lst2 lst3 lst4
-checkFourAcrosss _ _ _ _ = None
+checkFourAcrosss _ _ _ _ = Ongoing
 
 -- helper functions that checks if we have a winner
 orW :: Winner -> Winner -> Winner
-orW None w2 = w2
+orW Ongoing w2 = w2
 orW w1 w2 = w1
 
 -- sends four horizontal pieces to checkFourAcross function
 helperHorizontal :: [[Position]] -> Winner
 helperHorizontal (lst1:lst2:lst3:lst4:xs) = orW winner (helperHorizontal (lst2:lst3:lst4:xs))
    where winner = checkFourAcross lst1 lst2 lst3 lst4
-helperHorizontal (_:xs) = None
+helperHorizontal (_:xs) = Ongoing
 
 -- sends four diagonal pieces starting from top left to bottom right to checkFourAcross function
 helperDiagonalDown :: [[Position]] -> Winner
 helperDiagonalDown (lst1:lst2:lst3:lst4:xs) = orW winner (helperDiagonalDown (lst2:lst3:lst4:xs))
    where winner = checkFourAcross lst1 (drop 1 lst2) (drop 2 lst3) (drop 3 lst4)
-helperDiagonalDown (_:xs) = None
+helperDiagonalDown (_:xs) = Ongoing
 
 -- sends four diagonal pieces starting from bottom left to top right to checkFourAcross function
 helperDiagonalUp :: [[Position]] -> Winner
 helperDiagonalUp (lst1:lst2:lst3:lst4:xs) = orW winner (helperDiagonalUp (lst2:lst3:lst4:xs))
    where winner = checkFourAcross (drop 3 lst1) (drop 2 lst2) (drop 1 lst3) lst4
-helperDiagonalUp (_:xs) = None
+helperDiagonalUp (_:xs) = Ongoing
 
 blankBoard = replicate 7 (replicate 6 Empty)
 
