b2760d2f0a17f06b237cf4b673a80d63f1ab9668
Christian Walters <christian@Christians-Mac-Daddy.local>
2024-12-02 12:20:06 -0600

    story 21 done? maybe?

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,37 @@ import Connect4
 import System.IO
 import System.Environment
 import System.Directory
+import System.Console.GetOpt
+
+data Flag = NA deriving (Show,Eq)
+
+options :: [OptDescr Flag]
+options = [Option ['n'] ["NA"] (NoArg NA) "Temp flag while empty"]
 
 -- asks for a file name and returns the game, the best move, and the outcome of that move
-main :: IO() 
+main :: IO()
 main =
    do args <- getArgs
-      -- chatGPT used to help with putting folders in the right place and getting correct path
-      let folderPath = "../testFiles"
-      fName <- prompt "Enter a file name"
-      let fullPath = folderPath ++ "/" ++ fName
-      game <- loadGame fullPath
-      putStrLn $ prettyPrint game
-      putBestMove game
+      let (flags,inputs,errors) = getOpt Permute options args
+      putStrLn $ show (flags,inputs,errors)
+      if NA `elem` flags
+      then print "temp placeholder"
+      else
+        do
+          -- chatGPT used to help with putting folders in the right place and getting correct path
+          let folderPath = "../testFiles"
+          fName <- prompt "Enter a file name"
+          let fullPath = folderPath ++ "/" ++ fName
+          game <- loadGame fullPath
+          putStrLn $ prettyPrint game
+          dispatch flags game
+          --putBestMove game
 
+dispatch flags game 
+  | otherwise = putBestMove game
 -- helper function to ask for user input 
 prompt :: String -> IO String
-prompt str = 
+prompt str =
   do putStr $ str ++ ": "
      hFlush stdout
      answer <- getLine
@@ -27,14 +42,14 @@ prompt str =
 
 -- takes a game and file path and puts the game in the file
 writeGame :: Game -> FilePath -> IO ()
-writeGame game file = 
+writeGame game file =
   let stringGame = showGame game
   in writeFile file stringGame
 
 -- takes a file and returns the game in the file
-loadGame :: FilePath -> IO Game 
-loadGame file = 
-  do 
+loadGame :: FilePath -> IO Game
+loadGame file =
+  do
   contents <- readFile file
   return $ readGame contents
 
